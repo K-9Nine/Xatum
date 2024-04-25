1c06247308bc770e2d34335d1c73ee443b51667e
K9 <99043728+K-9Nine@users.noreply.github.com>
2024-04-25 21:43:31 +0100

    Update miner.go

--- xatum-reference-miner/cmd/miner/miner.go
+++ xatum-reference-miner/cmd/miner/miner.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"sync"
 	"time"
+
 	"xatum-reference-miner/config"
 	"xatum-reference-miner/log"
 	"xatum-reference-miner/xatum"
@@ -13,38 +14,36 @@ import (
 	"xatum-reference-miner/xelisutil"
 )
 
-// Job is a fast & efficient struct used for storing a job in memory
 type Job struct {
 	Blob   xelisutil.BlockMiner
 	Diff   uint64
 	Target [32]byte
 }
 
-var cl *client.Client
-var connected = false
-
-func main() {
-
-	start = time.Now()
-
-	for i := 0; i < 4; i++ {
-		go miningThread()
-	}
-
-	go logHashrate()
-
-	clientHandler()
+type Miner struct {
+	client    *client.Client
+	connected bool
+	currentJob Job
+	jobMutex   sync.RWMutex
+	hashCount  float64
+	startTime  time.Time
 }
 
-func clientHandler() {
+func NewMiner() *Miner {
+	return &Miner{
+		startTime: time.Now(),
+	}
+}
+
+func (m *Miner) clientHandler() {
 	for {
-		var err error
-		cl, err = client.NewClient("127.0.0.1:6969")
+		cl, err := client.NewClient("127.0.0.1:6969")
 		if err != nil {
 			log.Errf("%v", err)
 			time.Sleep(time.Second)
 			continue
 		}
+		m.client = cl
 
 		err = cl.Send(xatum.PacketC2S_Handshake, xatum.C2S_Handshake{
 			Addr:  config.ADDRESS,
@@ -58,96 +57,54 @@ func clientHandler() {
 			continue
 		}
 
-		go readjobs(cl.Jobs)
+		go m.readJobs(cl.Jobs)
 		cl.Connect()
 
 		time.Sleep(time.Second)
 	}
 }
 
-var numHashes float64
-var start time.Time
-var curJob Job
-var mutCurJob sync.RWMutex
-
-func readjobs(clJobs chan xatum.S2C_Job) {
+func (m *Miner) readJobs(clJobs chan xatum.S2C_Job) {
 	for {
 		job, ok := <-clJobs
 		if !ok {
 			return
 		}
 
-		mutCurJob.Lock()
-		connected = true
-
-		curJob = Job{
+		m.jobMutex.Lock()
+		m.connected = true
+		m.currentJob = Job{
 			Blob:   xelisutil.BlockMiner(job.Blob),
 			Diff:   job.Diff,
 			Target: xelisutil.GetTargetBytes(job.Diff),
 		}
-		mutCurJob.Unlock()
+		m.jobMutex.Unlock()
 
 		log.Infof("new job: diff %d, blob %x", job.Diff, job.Blob)
-
 	}
 }
 
-func miningThread() {
-	var scratch = xelishash.ScratchPad{}
-
+func (m *Miner) logHashrate() {
 	for {
-		if !connected {
-			time.Sleep(time.Second)
-			continue
+		m.jobMutex.Lock()
+		elapsed := time.Since(m.startTime).Seconds()
+		if elapsed == 0 {
+			elapsed = 1 // prevent division by zero
 		}
-
-		if cl == nil {
-			log.Err("client is nil")
-			time.Sleep(time.Second)
-			continue
-		}
-
-		mutCurJob.Lock()
-		numHashes++
-		nonce := curJob.Blob.GetNonce()
-		if nonce == 0xffffffffffffffff {
-			nonce = 0
-		} else {
-			nonce++
-		}
-
-		curJob.Blob.SetNonce(nonce)
-		job := curJob
-		mutCurJob.Unlock()
-
-		hash := job.Blob.PowHash(&scratch)
-
-		if bytes.Compare(hash[:], job.Target[:]) < 0 {
-			log.Infof("found block with PoW %x", hash)
-			cl.Lock()
-
-			cl.Submit(xatum.C2S_Submit{
-				Data: job.Blob[:],
-				Hash: hex.EncodeToString(hash[:]),
-			})
-
-			cl.Unlock()
-
-		}
-	}
-}
-
-func logHashrate() {
-	for {
-		mutCurJob.Lock()
-
-		log.Infof("hashrate: %.0f", numHashes/float64(time.Since(start).Milliseconds()/1000))
-
-		numHashes = 0
-		start = time.Now()
-
-		mutCurJob.Unlock()
+		log.Infof("hashrate: %.0f", m.hashCount/elapsed)
+		m.hashCount = 0
+		m.startTime = time.Now()
+		m.jobMutex.Unlock()
 
 		time.Sleep(10 * time.Second)
 	}
+}
+
+func main() {
+	miner := NewMiner()
+	for i := 0; i < 4; i++ {
+		go miner.miningThread()
+	}
+	go miner.logHashrate()
+	miner.clientHandler()
 }
